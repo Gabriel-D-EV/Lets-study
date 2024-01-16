8e7696b5fe805a938553bdaca70227ee1a82b324
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-16 19:28:04 -0300

    att sair

--- usuarios/views.py
+++ usuarios/views.py
@@ -5,47 +5,56 @@ from django.contrib.messages import constants
 from django.contrib import messages
 from django.contrib import auth
 
+
 def cadastro(req):
-    if req.method == 'GET':
-        return render(req, 'cadastro.html')
-    elif req.method == 'POST':
-        nome = req.POST.get('nome')
-        senha = req.POST.get('senha')
-        c_senha = req.POST.get('confirmar_senha')
-        
-        
+    if req.method == "GET":
+        return render(req, "cadastro.html")
+    elif req.method == "POST":
+        nome = req.POST.get("nome")
+        senha = req.POST.get("senha")
+        c_senha = req.POST.get("confirmar_senha")
+
         if not senha == c_senha:
-            messages.add_message(req, constants.ERROR, 'Senha e Confirmar senha não coincidem.')
-            return redirect('/usuarios/cadastro')
-        
+            messages.add_message(
+                req, constants.ERROR, "Senha e Confirmar senha não coincidem."
+            )
+            return redirect("/usuarios/cadastro")
+
         user = User.objects.filter(username=nome)
         if user.exists():
-            messages.add_message(req, constants.ERROR, 'Nome já existe!! ')
-            return redirect('/usuarios/cadastro')
+            messages.add_message(req, constants.ERROR, "Nome já existe!! ")
+            return redirect("/usuarios/cadastro")
 
         try:
             User.objects.create_user(username=nome, password=senha)
-            return redirect('/usuarios/logar')
-    
+            return redirect("/usuarios/logar")
+
         except:
-            messages.add_message(req, constants.ERROR, 'Erro interno do Server.')
-            return redirect('/usuarios/cadastro')
-        
-        
+            messages.add_message(req, constants.ERROR, "Erro interno do Server.")
+            return redirect("/usuarios/cadastro")
+
+
 def logar(req):
-    if req.method == 'GET':
-        return render(req, 'login.html')
-    elif req.method == 'POST':
-        nome = req.POST.get('nome')
-        senha = req.POST.get('senha')
+    if req.method == "GET":
+        return render(req, "login.html")
+    elif req.method == "POST":
+        nome = req.POST.get("nome")
+        senha = req.POST.get("senha")
+
+        user = auth.authenticate(req, username=nome, password=senha)
         
-        user = auth.authenticate(req)
-        if user.exists():
-            if user[0].check_password(senha):
-                return redirect('/usuarios/home')
-            else:
-                messages.add_message(req, constants.ERROR, 'Senha incorreta!!')
-                return redirect('/usuarios/login')
+        if user:
+            auth.login(req, user)
+            messages.add_message(req, constants.SUCCESS, "Logado!")
+            return redirect('/flashcard/novo_flashcard/')
         else:
-            messages.add_message(req, constants.ERROR, 'Usuário não encontrado!!')
-            return redirect('/usuarios/login')
+            messages.add_message(req, constants.ERROR, "Usuário ou senha incorreto.")
+            return redirect("/usuarios/logar")
+        
+def sair(req):
+    auth.logout(req)
+    return redirect('/usuarios/logar/')       
+        
+        
+        
+        
