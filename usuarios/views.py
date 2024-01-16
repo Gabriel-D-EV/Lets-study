6e04812492469076a2685e4786b4fa0221ddc7e1
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-16 11:18:35 -0300

    att

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 def cadastro(req):
-    return render(req, 'cadastro.html')
+    if req.method == 'GET':
+        return render(req, 'cadastro.html')
+    elif req.method == 'POST':
+        nome = req.POST.get('nome')
+        senha = req.POST.get('senha')
+        c_senha = req.POST.get('confirmar_senha')
+        if not senha == c_senha:
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.filter(username=nome)
+        if user.exists():
+            return redirect('/usuarios/cadastro')
+
+        try:
+            User.objects.create_user(username=nome, password=senha)
+            return redirect('/usuarios/login')
+    
+        except:
+            return redirect('/usuarios/cadastro')
+        
+        
+    
+        return HttpResponse("success")
