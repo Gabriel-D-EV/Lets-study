47865d2c14064d694dadcf898c2c45d201340b68
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-17 16:05:49 -0300

    flashcard

--- flashcard/views.py
+++ flashcard/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Categoria
 
-# Create your views here.
+def novo_flashcard(req):
+    if not req.user.is_authenticated:
+        return redirect("/usuarios/logar")
+    elif req.method =="GET":
+        categoria = Categoria.objects.all()
+        return render(req, "novo_flashcard.html")
+    
