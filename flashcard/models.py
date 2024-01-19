5fb1fbdc950a8f0637b1bba66ca5793c5cf304db
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-18 22:23:34 -0300

    f por hj 2.49

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,30 +1,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Categoria(models.Model):
     nome = models.CharField(max_length=25)
-    
+
     def __str__(self):
         return self.nome
- 
- 
+
+
 class Flashcard(models.Model):
-    DIFICULDADE_CHOICES = (('D', 'Difícil'), ('M', 'Médio'), ('F', 'Fácil'))
+    DIFICULDADE_CHOICES = (("D", "Difícil"), ("M", "Médio"), ("F", "Fácil"))
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     pergunta = models.CharField(max_length=200)
     resposta = models.TextField()
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
     dificuldade = models.CharField(max_length=1, choices=DIFICULDADE_CHOICES)
- 
+
     def __str__(self):
         return self.pergunta
+
+    @property
+    def css_dif(self):
+        if self.dificuldade == "F":
+            return "flashcard-facil"
+        elif self.dificuldade == "M":
+            return "flashcard-medio"
+        elif self.dificuldade == "D":
+            return "flashcard-dificil"
 
 
 class FlashcardDesafio(models.Model):
     flashcard = models.ForeignKey(Flashcard, on_delete=models.DO_NOTHING)
     respondido = models.BooleanField(default=True)
     acertou = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.flashcard.pergunta
 
@@ -34,12 +44,8 @@ class Desafio(models.Model):
     titulo = models.CharField(max_length=100)
     categoria = models.ManyToManyField(Categoria)
     q_perguntas = models.IntegerField()
-    dificuldades = models.CharField(
-        max_length=1, choices=Flashcard.DIFICULDADE_CHOICES
-    )
+    dificuldades = models.CharField(max_length=1, choices=Flashcard.DIFICULDADE_CHOICES)
     flashcard = models.ManyToManyField(FlashcardDesafio)
-    
+
     def __str__(self):
         return self.titulo
-    
-    
--- templates/static/flashcard/js/index.js
+++ templates/static/flashcard/js/index.js
@@ -1,7 +1,7 @@
 function flip_card(id) {
   card = document.getElementById(id);
 
-  if (card.style.display == "none") {
+  if (card.style.display == "none" || card.style.display == "") {
     card.style.display = "block";
   } else {
     card.style.display = "none";
