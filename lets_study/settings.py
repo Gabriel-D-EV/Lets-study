50afb50f953381510ab6e7e2c62d3c8a7cfa11be
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-01-22 22:45:03 -0300

    att bd, mudando para postgreesql

--- lets_study/settings.py
+++ lets_study/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'lets_study.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'lets_study',
+        'USER': 'postgres',
+        'PASSWORD': '051518',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
