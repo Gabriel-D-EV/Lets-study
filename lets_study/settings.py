9d173e2fd430e11cf549f3862ea8571ab7344b5d
Gabriel Silva <gabriel.silva_dev@outlook.com>
2024-02-25 19:07:44 -0300

    att

--- lets_study/settings.py
+++ lets_study/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'lets_study.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'lets_study',
@@ -86,6 +86,12 @@ DATABASES = {
         'PASSWORD': '051518',
         'HOST': 'localhost',
         'PORT': '5432',
+    },
+}'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
